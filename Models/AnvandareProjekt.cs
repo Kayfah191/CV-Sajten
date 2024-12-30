7cdfc6a544ba79b48fb2486349953e28b4997cda
Kayfah191 <kayhan.fa@hotmail.com>
2024-12-30 13:36:44 +0100

    AnvandareProjekt

---
+++ Models/AnvandareProjekt.cs
@@ -0,0 +1,13 @@
+﻿namespace CV_Sajten.Models
+{
+    public class AnvandareProjekt
+    { public int ID { get; set; }
+    public string Roll { get; set; }
+
+        public int AvsandareID { get; set; }
+        public int ProjektID { get; set; }
+
+        public Anvandare Avsandare { get; set; }
+        public Projekt Projekt { get; set; }
+    }
+}
