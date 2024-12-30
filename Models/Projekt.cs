b6401b20a308d8c07865c3dacda8da83e7c53ebf
andreassoldal <153736851+andreassoldal@users.noreply.github.com>
2024-12-30 13:17:57 +0100

    Create Projekt.cs

---
+++ Models/Projekt.cs
@@ -0,0 +1,10 @@
+﻿namespace CV_Sajten.Models
+{
+    public class Projekt
+    {
+        public int ID { get; set}
+        public string Titel { get;set; } 
+        public string Beskrivning { get; set; } 
+        public string Agare { get; set; }     
+    }
+}
