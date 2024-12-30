25b963234af97f5773781887e518d85ef786bc8a
danneboj <132871904+danneboj@users.noreply.github.com>
2024-12-30 13:20:30 +0100

    id

--- Models/Projekt.cs
+++ Models/Projekt.cs
@@ -2,9 +2,9 @@
 {
     public class Projekt
     {
-        public int ID { get; set}
+        public int ID { get; set; }
         public string Titel { get;set; } 
         public string Beskrivning { get; set; } 
-        public string Agare { get; set; }     
+        public int AnvandareID { get; set; }     
     }
 }
