05bccfa7fd206463778525aa261a0cf86176455d
danneboj <132871904+danneboj@users.noreply.github.com>
2024-12-30 13:14:35 +0100

    fälten

--- Models/Anvandare.cs
+++ Models/Anvandare.cs
@@ -2,5 +2,14 @@
 {
     public class Anvandare
     {
-    }
+
+        public int ID { get; set; }
+        public string Namn { get; set; }
+
+        public string Email { get; set; }
+
+        public Boolean isPrivat { get; set; }
+          
+        public string BildAdress { get; set; } 
+      }
 }
