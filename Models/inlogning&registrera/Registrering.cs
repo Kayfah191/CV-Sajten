688917d47329f8d9fbf31f2d55e71398e2b5efc3
Kayfah191 <kayhan.fa@hotmail.com>
2025-01-03 15:08:13 +0100

    registrering

--- Models/inlogning&registrera/Registrering.cs
+++ Models/inlogning&registrera/Registrering.cs
@@ -3,17 +3,37 @@
 namespace CV_Sajten.Models
 {
 	public class Registrering
-	{
-		[Required(ErrorMessage = "Vänligen skriv ett användarnamn.")]
-		[StringLength(255)]
-		public string AnvandarNamn { get; set; }
-		[Required(ErrorMessage = "Vänligen skriv lösenord.")]
-		[DataType(DataType.Password)]
-		[Compare("BekraftaLosenord")]
-		public string Losenord { get; set; }
-		[Required(ErrorMessage = "Vänlingen bekräfta lösenordet")]
-		[DataType(DataType.Password)]
-		[Display(Name = "Bekrafta losenordet")]
-		public string BekraftaLosenord { get; set; }
-	}
+    {
+		
+ [Required(ErrorMessage = "Vänligen skriv ditt namn.")]
+
+        [StringLength(255, ErrorMessage = "Namnet får inte vara längre än 255 tecken.")]
+        public string Namn { get; set; }
+        [Required(ErrorMessage = "Vänligen skriv ett användarnamn.")]
+        [StringLength(255)]
+        public string AnvandarNamn { get; set; }
+
+        [Required(ErrorMessage = "Vänligen skriv ett lösenord.")]
+        [DataType(DataType.Password)]
+        [Compare("BekraftaLosenord", ErrorMessage = "Lösenorden matchar inte.")]
+        public string Losenord { get; set; }
+
+        [Required(ErrorMessage = "Vänligen bekräfta lösenordet.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Bekräfta lösenordet")]
+        public string BekraftaLosenord { get; set; }
+
+       
+
+        [Required(ErrorMessage = "Vänligen skriv din e-postadress.")]
+        [EmailAddress(ErrorMessage = "Ogiltig e-postadress.")]
+        public string Epost { get; set; }
+
+        [Required(ErrorMessage = "Vänligen skriv ditt telefonnummer.")]
+        [Phone(ErrorMessage = "Ogiltigt telefonnummer.")]
+        [Display(Name = "Telefonnummer")]
+        public string TelefonNummer { get; set; }
+    }
 }
+
+
